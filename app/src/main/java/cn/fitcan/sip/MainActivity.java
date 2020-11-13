1300c0a5cf7adccc5ae629a7fe2cc8762807306d
YUxiangLuo <871884617@qq.com>
2020-11-13 15:30:06 +0800

    根据别人的库换了代码，申请了很多权限，还是没声音

--- app/src/main/java/cn/fitcan/sip/MainActivity.java
+++ app/src/main/java/cn/fitcan/sip/MainActivity.java
@@ -10,6 +10,11 @@ import android.net.sip.SipManager;
 import android.os.Bundle;
 
 import org.pjsip.pjsua2.*;
+
+import java.util.logging.Logger;
+
+import static org.pjsip.pjsua2.pjmedia_type.PJMEDIA_TYPE_AUDIO;
+
 // Subclass to extend the Account and get notifications etc.
 class MyAccount extends Account {
     @Override
@@ -18,67 +23,112 @@ class MyAccount extends Account {
     }
 }
 
-class MyCall extends Call {
-    public MyCall(MyAccount myAccount, int id) {
-        super(myAccount, id);
-    }
-
-    @Override
-    public void onCallState(OnCallStateParam prm) {
-        super.onCallState(prm);
-        try {
-            CallInfo callInfo = getInfo();
-
-            int role = callInfo.getRole();
-            if(role == pjsip_role_e.PJSIP_ROLE_UAC) {
-                System.out.println("==============呼出：状态变更====================");
-            }else if(role == pjsip_role_e.PJSIP_ROLE_UAS) {
-                System.out.println("==============呼入：状态变更====================");
-            }
-
-            int state = callInfo.getState();
-            if (state == pjsip_inv_state.PJSIP_INV_STATE_CALLING) {
-                System.out.println("==============正在呼出====================");
-            } else if (state == pjsip_inv_state.PJSIP_INV_STATE_EARLY) {
-                System.out.println("==============对方正在响铃====================");
-            } else if (state == pjsip_inv_state.PJSIP_INV_STATE_CONNECTING) {
-                System.out.println("==============连接成功====================");
-            } else if (state == pjsip_inv_state.PJSIP_INV_STATE_CONFIRMED) {
-                System.out.println("==============通话中====================");
-            } else if (state == pjsip_inv_state.PJSIP_INV_STATE_DISCONNECTED) {
-                System.out.println("==============挂断====================");
-            }
-
-        }catch (Exception e) {
-            System.out.println(e);
-            return;
-        }
-    }
-
-    @Override
-    public void onCallMediaState(OnCallMediaStateParam prm) {
-        super.onCallMediaState(prm);
-        CallInfo callInfo;
-        try {
-            callInfo = getInfo();
-        }catch (Exception e) {
-            System.out.println(e);
-            return;
-        }
-        // try to convert MyCall to inner class to get ep then get audDevice Manager
-    }
-}
-
-
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private boolean permissionToRecordAccepted = false;
-    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.INTERNET, Manifest.permission.MODIFY_AUDIO_SETTINGS, Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.WAKE_LOCK, Manifest.permission.VIBRATE, Manifest.permission.USE_SIP, Manifest.permission.CAMERA};
 
     private Endpoint ep;
     private MyAccount acc;
+
+    class MyCall extends Call {
+        public MyCall(MyAccount myAccount, int id) {
+            super(myAccount, id);
+        }
+
+        @Override
+        public void onCallState(OnCallStateParam prm) {
+            super.onCallState(prm);
+            try {
+                CallInfo callInfo = getInfo();
+
+                int role = callInfo.getRole();
+                if(role == pjsip_role_e.PJSIP_ROLE_UAC) {
+                    System.out.println("==============呼出：状态变更====================");
+                }else if(role == pjsip_role_e.PJSIP_ROLE_UAS) {
+                    System.out.println("==============呼入：状态变更====================");
+                }
+
+                int state = callInfo.getState();
+                if (state == pjsip_inv_state.PJSIP_INV_STATE_CALLING) {
+                    System.out.println("==============正在呼出====================");
+                } else if (state == pjsip_inv_state.PJSIP_INV_STATE_EARLY) {
+                    System.out.println("==============对方正在响铃====================");
+                } else if (state == pjsip_inv_state.PJSIP_INV_STATE_CONNECTING) {
+                    System.out.println("==============连接成功====================");
+                } else if (state == pjsip_inv_state.PJSIP_INV_STATE_CONFIRMED) {
+                    System.out.println("==============通话中====================");
+                } else if (state == pjsip_inv_state.PJSIP_INV_STATE_DISCONNECTED) {
+                    System.out.println("==============挂断====================");
+                }
+
+            }catch (Exception e) {
+                System.out.println(e);
+                return;
+            }
+        }
+
+        @Override
+        public void onCallMediaState(OnCallMediaStateParam prm) {
+            System.out.println("..............................on call media state.............................");
+
+
+            CallInfo info;
+            try {
+                info = getInfo();
+            } catch (Exception exc) {
+                System.out.println("onCallMediaState: error while getting call info");
+                return;
+            }
+
+            for (int i = 0; i < info.getMedia().size(); i++) {
+                Media media = getMedia(i);
+                CallMediaInfo mediaInfo = info.getMedia().get(i);
+
+                if (mediaInfo.getType() == pjmedia_type.PJMEDIA_TYPE_AUDIO
+                        && media != null
+                        && mediaInfo.getStatus() == pjsua_call_media_status.PJSUA_CALL_MEDIA_ACTIVE) {
+
+                    handleAudioMedia(media);
+
+                } else if (mediaInfo.getType() == pjmedia_type.PJMEDIA_TYPE_VIDEO
+                        && mediaInfo.getStatus() == pjsua_call_media_status.PJSUA_CALL_MEDIA_ACTIVE
+                        && mediaInfo.getVideoIncomingWindowId() != pjsua2.INVALID_ID) {
+
+                    handleVideoMedia(mediaInfo);
+                }
+            }
+
+        }
+    }
     private MyCall myCall;
+
+    private void handleAudioMedia(Media media) {
+        AudioMedia audioMedia = AudioMedia.typecastFromMedia(media);
+
+        // connect the call audio media to sound device
+        try {
+            AudDevManager audDevManager = ep.audDevManager();
+            if (audioMedia != null) {
+                try {
+                    audioMedia.adjustRxLevel((float) 1.5);
+                    audioMedia.adjustTxLevel((float) 1.5);
+                } catch (Exception exc) {
+                    System.out.println("Error while adjusting levels");
+                }
+
+                audioMedia.startTransmit(audDevManager.getPlaybackDevMedia());
+                audDevManager.getCaptureDevMedia().startTransmit(audioMedia);
+            }
+        } catch (Exception exc) {
+            System.out.println("Error while connecting audio media to sound device");
+        }
+    }
+
+    private void handleVideoMedia(CallMediaInfo mediaInfo) {
+        System.out.println("================Handle VIDEO MEDIA============");
+    }
 
 
     @Override
@@ -113,7 +163,11 @@ public class MainActivity extends AppCompatActivity {
             acc = new MyAccount();
             acc.create(acfg);
             myCall = new MyCall(acc, pjsua_invalid_id_const_.PJSUA_INVALID_ID);
-            CallOpParam prm = new CallOpParam(true);
+            CallOpParam prm = new CallOpParam();
+            CallSetting callSetting = prm.getOpt();
+            callSetting.setAudioCount(1);
+            callSetting.setVideoCount(0);
+            callSetting.setFlag(pjsua_call_flag.PJSUA_CALL_INCLUDE_DISABLED_MEDIA);
             myCall.makeCall("sip:4002@192.168.1.32", prm);
             System.out.println("call call call call");
         }catch (Exception e) {
